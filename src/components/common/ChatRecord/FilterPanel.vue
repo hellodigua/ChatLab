eea39b899811dce7b6592b8287fca377f1946ea0
xuncha <1658671838@qq.com>
2026-01-31 16:05:51 +0800

    fix: 修复时间选择器筛选

--- src/components/common/ChatRecord/FilterPanel.vue
+++ src/components/common/ChatRecord/FilterPanel.vue
@@ -6,7 +6,7 @@
 import { ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import dayjs from 'dayjs'
-import { DatePicker } from '@/components/UI'
+import DatePickerPopover from './DatePickerPopover.vue'
 import type { ChatRecordQuery, FilterFormData } from './types'
 
 const { t } = useI18n()
@@ -123,9 +123,17 @@ function resetFilter() {
       <UInput v-model="formData.messageId" type="number" :placeholder="t('messageId')" size="sm" class="w-24" />
       <UInput v-model="formData.memberName" :placeholder="t('memberNotSupported')" size="sm" class="w-28" disabled />
       <div class="flex items-center gap-2">
-        <DatePicker v-model="formData.startDate" :placeholder="t('startDate')" />
+        <DatePickerPopover
+          v-model="formData.startDate"
+          :placeholder="t('startDate')"
+          :max="formData.endDate || undefined"
+        />
         <span class="text-xs text-gray-400">~</span>
-        <DatePicker v-model="formData.endDate" :placeholder="t('endDate')" />
+        <DatePickerPopover
+          v-model="formData.endDate"
+          :placeholder="t('endDate')"
+          :min="formData.startDate || undefined"
+        />
       </div>
     </div>
 
@@ -156,7 +164,7 @@ function resetFilter() {
     "messageId": "消息 ID",
     "memberNotSupported": "成员（暂不支持）",
     "keywordsPlaceholder": "关键词，多个用逗号分隔，回车搜索",
-    "startDate": "起始日期",
+    "startDate": "开始日期",
     "endDate": "结束日期",
     "reset": "重置",
     "filter": "筛选"
@@ -165,8 +173,8 @@ function resetFilter() {
     "messageId": "Message ID",
     "memberNotSupported": "Member (not supported)",
     "keywordsPlaceholder": "Keywords, comma separated, Enter to search",
-    "startDate": "Start Date",
-    "endDate": "End Date",
+    "startDate": "Start date",
+    "endDate": "End date",
     "reset": "Reset",
     "filter": "Filter"
   }
